bb91849a2a0bdddf490b058c285a0b0c7d84030c
donatas <petrikauskas.donatas@gmail.com>
2022-05-08 14:52:53 +0300

    offer lists

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         \App\Models\User::factory(1000)->create();
+         \App\Models\User::factory(1000)->create();
          \App\Models\Category::factory(100)->create();
 
          $this->call(OfferSeeder::class);
