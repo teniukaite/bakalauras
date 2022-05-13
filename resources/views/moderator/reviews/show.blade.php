049779098ecfd8e9e22ee527e13d447d1a0d9d3f
Teniukaite <karolina.teniukaite@scoding.com>
2022-05-13 10:57:27 +0300

    fixes

--- resources/views/moderator/reviews/show.blade.php
+++ resources/views/moderator/reviews/show.blade.php
@@ -66,6 +66,39 @@
                                                 <strong>Paslaugos pasiūlymas:</strong>
                                                {{$review->offersReview->service_name}}
                                             </div>
+                                            <div data-aos="fade-right" data-aos-delay="200" data-aos-duration="1000" data-aos-once="true" class="col-md-4 text-center py-4 mt-5">
+                                                <h4 class="my-4">{{ $review->offersReview->service_name }}</h4>
+                                                <div class="row justify-content-center">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <div id="carouselExampleControls-<?=$review->offersReview->id;?>" class="carousel slide" data-ride="carousel">
+                                                                <div class="carousel-inner">
+                                                                    @foreach($review->offersReview->files as $file)
+                                                                        <div class="carousel-item">
+                                                                            <img class="d-block w-100" src="{{ asset($file->file_path) }}" alt="file">
+                                                                        </div>
+                                                                    @endforeach
+                                                                </div>
+                                                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls-<?=$review->offersReview->id;?>" data-bs-slide="prev">
+                                                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                                    <span class="visually-hidden">Previous</span>
+                                                                </button>
+                                                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls-<?=$review->offersReview->id;?>" data-bs-slide="next">
+                                                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                                                    <span class="visually-hidden">Next</span>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <p class="font-weight-bold">€ <span class="display-2 font-weight-bold">{{ $review->offersReview->price }}</span> {{ $review->offersReview->price_content }}</p>
+                                                <ul class="list-unstyled">
+                                                    <li><i class="fa-solid fa-map-location-dot"></i> {{ $review->offersReview->cities->name }}</li>
+                                                    <li><i class="fa-solid fa-clock"></i> {{ $review->offersReview->duration }}</li>
+                                                    <li><i class="fa-solid fa-user"></i> {{$review->offersReview->freelancer->name}} {{$review->offersReview->freelancer->lastName}}</li>
+                                                </ul>
+                                                <a href="{{route('offers.show', $review->offersReview->id)}}" class="btn my-4 font-weight-bold atlas-cta cta-ghost">Peržiūrėti</a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -82,4 +115,7 @@
 @endsection
 
 @section('js')
+    <script>
+        $('.carousel-inner > .carousel-item:first-child').addClass('active');
+    </script>
 @endsection
